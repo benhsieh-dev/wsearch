0750daf47a4def07671ed76fffe71e905f6a5a9a
Ben Hsieh <bqh5026@gmail.com>
2021-07-23 18:36:38 -0500

    Turn off even null check in tsconfig.json

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,5 @@
-// import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  // @Output() submitted = new EventEmitter<string>();
+  @Output() submitted = new EventEmitter<string>();
   term = ''; 
 
   constructor() { }
@@ -19,8 +19,8 @@ export class SearchBarComponent implements OnInit {
   // onFormSubmit(term: any) {
     onFormSubmit(event: any) {
     event.preventDefault();
-    console.log(this.term);
-    // this.submitted.emit(this.term); 
+    // console.log(this.term);
+    this.submitted.emit(this.term); 
   }
   
 }
